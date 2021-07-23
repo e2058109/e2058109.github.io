573e76a8accebfefc42c2ebf7b2ebe3d566ce5cf
e2058109 <87455031+e2058109@users.noreply.github.com>
2021-07-23 11:26:29 +0900

    Add files via upload

--- js/base.js
+++ js/base.js
@@ -15,4 +15,20 @@ window.onload = function() {
         '#000', 'rgba(204,0,0,.225)', 'rgba(0,187,221,.2)'
       ]
     });
-  };
+} ;
+
+var pics_src = new Array("./img/th.jfif","./img/Dead-by-Deadlight.jpg","./img/z_5dd752e3237a7.jpg");
+var num = -1;
+ 
+slideshow_timer();
+
+function slideshow_timer(){
+    if (num == 2){
+        num = 0;
+    } 
+    else {
+        num ++;
+    }
+    document.getElementById("mypic").src=pics_src[num];
+    setTimeout("slideshow_timer()",1000); 
+}
